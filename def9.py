f5e95596bd1ffe692ba309e445ede9e8cf9bf6ed
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-13 23:59:38 -0300

    Update def9.py

--- def9.py
+++ def9.py
@@ -8,8 +8,13 @@ funções def em  Python
 
 def func(*args, **kwargs):
      print(args)
-     print(kwargs["nome"], kwargs["idade"])
+     #print(kwargs["nome"], kwargs["idade"])
+     #ou:
+     nome = kwargs.get("nome")
+     idade = kwargs.get("idade")
+     print(nome, idade)
+
 
 lista = [1,2,3,4,5]
 lista2 = [250,500,750,1000]
-func(*lista, *lista2, nome = "Alan", idade = "31")
+func(*lista, *lista2, nome = "Alan", idade = 31)
