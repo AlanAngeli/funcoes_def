038ec21332f7fd3fd39d21fe9bed58f69499eb83
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-13 23:55:21 -0300

    Add files via upload

---
+++ def9.py
@@ -0,0 +1,15 @@
+"""
+funções def em  Python
+
+*args,**kwargs
+
+
+"""
+
+def func(*args, **kwargs):
+     print(args)
+     print(kwargs["nome"], kwargs["idade"])
+
+lista = [1,2,3,4,5]
+lista2 = [250,500,750,1000]
+func(*lista, *lista2, nome = "Alan", idade = "31")
