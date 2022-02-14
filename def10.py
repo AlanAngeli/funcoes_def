64f25734075536daa2dd473ee883e1ba1e2ba3d0
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-14 13:02:32 -0300

    Update def10.py

--- def10.py
+++ def10.py
@@ -2,13 +2,16 @@ def func(*args,**kwargs):
     print(args)
 
     idade = kwargs.get("idade")
+    nome = kwargs.get("nome")
 
-
+    print(nome)
     if idade is not None:
         print(idade)
     else:
         print("Idade não exste")
 
 lista = [1,2,3]
-func(*lista, idade = None)
+func(*lista, nome = "Alan", idade = 31)
+
+
 
