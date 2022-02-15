4b48d90f87b90a56344566867859275da91c333b
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-15 11:37:32 -0300

    Add files via upload

---
+++ def12.py
@@ -0,0 +1,15 @@
+""" Melhor maneira de substituir a variavel, não usando a função global"""
+
+var = "valor"
+
+def func():
+    print(var)
+
+def func2(arg=None):
+    arg = arg.replace("v","c")
+    return arg
+
+func()
+outra_var = func2(arg=var)
+
+print(outra_var)
