3e5ef06389f71f335d1a93501e31fad85ed80c44
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-20 23:32:03 -0300

    Update exercicios2_def.py

--- exercicios2_def.py
+++ exercicios2_def.py
@@ -1,7 +1,7 @@
 """
 
 Crie uma função1 que receba uma função2 como parâmetro e retorne o valor da função2 executada. Faça a função1 executar
-duas fynções que recebam um número diferente de argumentos.
+duas fynções que recebam um número diferente de argumentos
 
 """
 
